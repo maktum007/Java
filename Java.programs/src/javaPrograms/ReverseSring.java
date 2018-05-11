03bac34638b00a6e15b43055c6d5c22726dc44b8
maktum <maktum.080@gmail.com>
2018-05-11 21:39:57 +0530

    a

---
+++ Java.programs/src/javaPrograms/ReverseSring.java
@@ -0,0 +1,18 @@
+package javaPrograms;
+
+public class ReverseSring {
+
+	public static void main(String[] args) 
+	{
+		String s="read";
+		String rev="";
+		
+		for(int i=s.length()-1;i>=0;i--)
+		{
+			rev+=s.charAt(i);
+		}
+		System.out.println(rev);
+
+	}
+
+}
